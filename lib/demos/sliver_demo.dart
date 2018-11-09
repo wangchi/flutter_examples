d1037f12372f5cf6410e5a439c897bfb6e4d19cf
Wang Chi <hiwangchi@gmail.com>
2018-11-09 23:55:45 +0800

    add sliver demo

---
+++ lib/demos/sliver_demo.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tutorials/demos/listview.dart';
+
+class SliverDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: new AppBar(
+      //   title: Text('Flutter Tutorials'),
+      //   leading: IconButton(
+      //     icon: Icon(Icons.menu),
+      //     tooltip: 'Menu',
+      //     onPressed: () => debugPrint('Menu button pressed!'),
+      //   ),
+      //   actions: <Widget>[
+      //     IconButton(
+      //       icon: Icon(Icons.search),
+      //       tooltip: 'Search',
+      //       onPressed: () => debugPrint('Search button pressed!'),
+      //     ),
+      //   ],
+      // ),
+      // body: ListViewDemo()
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            // title: Text('PUSHSTACK'),
+            // pinned: true,
+            floating: true,
+            expandedHeight: 200.0,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'www.pushstack.com'.toUpperCase()
+              ),
+              background: Image.network(
+                'https://tva3.sinaimg.cn/crop.0.0.640.640.640/a1d3feabjw1eca1gkmhz8j20hs0hswfn.jpg',
+                fit: BoxFit.cover
+              ),
+            ),
+          ),
+          SliverSafeArea(
+            sliver: SliverPadding(
+              padding: EdgeInsets.all(10.0),
+              sliver: SliverGridDemo(),
+              // sliver: SliverListDemo(),
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
+
+class SliverGridDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0,
+        mainAxisSpacing: 8.0,
+        childAspectRatio: 1.0
+      ),
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        return Container(
+          child: Image.network(
+            'https://tva2.sinaimg.cn/crop.0.0.640.640.180/9e5b07c7jw8f014yvcyjbj20hs0hs3z4.jpg',
+            fit: BoxFit.cover
+          )
+        );
+      }),
+    );
+  }
+}
+
+
+class SliverListDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+        return Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Material(
+            borderRadius: BorderRadius.circular(10.0),
+            elevation: 10.0,
+            shadowColor: Colors.grey.withOpacity(0.5),
+            child: AspectRatio(
+              aspectRatio: 16/9,
+              child: Image.network(
+                'https://tva2.sinaimg.cn/crop.0.0.640.640.180/9e5b07c7jw8f014yvcyjbj20hs0hs3z4.jpg',
+                fit: BoxFit.cover
+              )
+            ),
+          ),
+        );
+      }),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'package:flutter_tutorials/demos/waterfall_demo3.dart';
 import 'package:flutter_tutorials/demos/staggered.dart';
 import 'package:flutter_tutorials/demos/bottomnavigationbar_demo.dart';
 import 'package:flutter_tutorials/pages/about.dart';
+import 'package:flutter_tutorials/demos/tabbar_demo.dart';
+import 'package:flutter_tutorials/demos/sliver_demo.dart';
 
 void main() {
   runApp(MyApp());
@@ -23,13 +25,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 //      home: Home(),
       // home: AppBarDemo(),
-      home: TabBarDemo(),
+      // home: TabBarDemo(),
       // home: DrawerDemo(),
-      //  home: BottomNavigationBarDemo(),
+       home: BottomNavigationBarDemo(),
       // home: MyLayoutDemo(),
       // home: FlowLayoutDemo(),
 //       home: WaterfallDemo3(),
       // home: Example08(),
+      // home: SliverDemo(),
       theme: ThemeData(
         // primarySwatch: Colors.green
         primaryColor: Color.fromRGBO(212, 61, 61, 1.0)
@@ -82,120 +85,6 @@ class AppBarDemo extends StatelessWidget {
           style: TextStyle(fontSize: 32.0)
         )
       )
-    );
-  }
-}
-
-// home: TabBarDemo()
-class TabBarDemo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: new Scaffold(
-        appBar: new AppBar(
-          title: Text('Flutter Tutorials'),
-          leading: IconButton(
-            icon: Icon(Icons.menu),
-            tooltip: 'Menu',
-            onPressed: () => debugPrint('Menu button pressed!'),
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: () => debugPrint('Search button pressed!'),
-            ),
-          ],
-          // bottom: PreferredSize(
-          //   preferredSize: const Size.fromHeight(300.0),
-          //   child: Container(
-          //     child: Column(
-          //       children: <Widget>[
-          //         Container(
-          //           margin: EdgeInsets.all(16.0),
-          //           width: double.infinity,
-          //           height: 200.0,
-          //           color: Colors.white,
-          //           child: Center(
-          //             child: Text('place something in appbar'),
-          //           ),
-          //         ),
-          //         TabBar(
-          //           indicatorColor: Colors.white,
-          //           tabs: <Widget>[
-          //             Tab(icon: Icon(Icons.mail)),
-          //             Tab(icon: Icon(Icons.image)),
-          //             Tab(icon: Icon(Icons.live_tv)),
-          //             Tab(icon: Icon(Icons.view_agenda)),
-          //           ]
-          //         )
-          //       ]
-          //     )
-          //   ),
-          // ),
-          bottom: TabBar(
-            indicatorColor: Colors.white,
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.mail)),
-              Tab(icon: Icon(Icons.image)),
-              Tab(icon: Icon(Icons.live_tv)),
-            ]
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            Icon(Icons.mail, size: 128.0, color: Colors.black12),
-            Icon(Icons.image, size: 128.0, color: Colors.black12),
-            // Icon(Icons.live_tv, size: 128.0, color: Colors.black12),
-            SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.all(16.0),
-                    width: double.infinity,
-                    height: 200.0,
-                    color: Colors.grey,
-                    child: Center(child: Text('Demo'),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(16.0),
-                    width: double.infinity,
-                    height: 200.0,
-                    color: Colors.grey,
-                    child: Center(child: Text('Demo'),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(16.0),
-                    width: double.infinity,
-                    height: 200.0,
-                    color: Colors.grey,
-                    child: Center(child: Text('Demo'),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(16.0),
-                    width: double.infinity,
-                    height: 200.0,
-                    color: Colors.grey,
-                    child: Center(child: Text('Demo'),),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(16.0),
-                    width: double.infinity,
-                    height: 200.0,
-                    color: Colors.grey,
-                    child: Center(child: Text('Demo'),),
-                  )
-                ],
-              ),
-            )
-            // LayoutDemo()
-          ]
-        )
-        // body: Hello()
-        // body: VideoApp()
-        // body: ListViewDemo()
-      ),
     );
   }
 }
